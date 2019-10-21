e186b9740783b6dc4c7109330f4e30ca6de8d9f3
Javier Peletier <jm@epiclabs.io>
2019-10-22 00:11:44 +0200

    adapt nodemcu-httpserver

---
+++ net/http/static.lua
@@ -0,0 +1,26 @@
+-- httpserver-static.lua
+-- Part of nodemcu-httpserver, handles sending static files to client.
+-- Author: Marcos Kirsch
+
+return function(connection, req, args)
+   require("net.http.header")(connection, 200, args.ext, args.isGzipped)
+   -- Send file in little chunks
+   local bytesRemaining = file.list()[args.file]
+   -- Chunks larger than 1024 don't work.
+   -- https://github.com/nodemcu/nodemcu-firmware/issues/1075
+   local chunkSize = 1024
+   local fileHandle = file.open(args.file)
+   while bytesRemaining > 0 do
+      local bytesToRead = math.min(bytesRemaining, chunkSize)
+      local chunk = fileHandle:read(bytesToRead)
+      connection:send(chunk)
+      bytesRemaining = bytesRemaining - #chunk
+      --print(args.file .. ": Sent "..#chunk.. " bytes, " .. bytesRemaining .. " to go.")
+      chunk = nil
+      collectgarbage()
+   end
+   -- print("Finished sending: ", args.file)
+   fileHandle:close()
+   fileHandle = nil
+   collectgarbage()
+end
