5812222318a83f185bfe51b82e5c5405acfae6a5
Javier Peletier <jm@epiclabs.io>
2019-10-21 11:39:16 +0200

    removed "switches" folder

--- state/count.lua
+++ state/count.lua
@@ -1,16 +1,22 @@
+-- TODO: adapt the below to be a state machine
+
 CountSwitch = {}
 function CountSwitch:new(dInput, interval)
-    local o = {count=0}
+    local o = {count = 0}
     setmetatable(o, self)
     self.__index = self
     o.timer = tmr.create()
-    o.timer:register(interval,tmr.ALARM_SEMI, function()
-        if o.callback ~= nil then
-            o.callback(o)
+    o.timer:register(
+        interval,
+        tmr.ALARM_SEMI,
+        function()
+            if o.callback ~= nil then
+                o.callback(o)
+            end
+            o.count = 0
         end
-        o.count=0
-    end)
-    
+    )
+
     dInput.callback = function(pinState)
         if pinState ~= 0 then
             o.count = o.count + 1
