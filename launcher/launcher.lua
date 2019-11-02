33d2ad061a1ccf8de64b676ecfae05c0abb01ec1
Javier Peletier <jm@epiclabs.io>
2019-11-02 19:39:27 +0100

    refactored launcher

--- launcher/launcher.lua
+++ launcher/launcher.lua
@@ -1,33 +1,58 @@
 -- datafile: app-config.json
+-- datafile: site-app-config.json
+
+local log = require("core.log"):new("launcher")
+local Launcher = {apps = {}}
+
 local function launchApp(App, appInfo)
-    local instance = App:new()
+    log:info("Launching %s ...", appInfo.name)
+    local ok, instance = pcall(App.new, App)
+    if not ok then
+        log:error("Error instantiating %s: %s", appInfo.name, instance)
+        return
+    end
     instance.name = appInfo.name
-    instance:init(appInfo.config)
+    local ok, err = pcall(instance.init, instance, appInfo.config)
+    if not ok then
+        log:error("Error initializing %s: %s", appInfo.name, err)
+        return
+    end
+    table.insert(Launcher.apps, instance)
+end
+
+local function launch(appInfo)
+    local ok, App = pcall(require, appInfo.module)
+    if ok then
+        ok, err = pcall(launchApp, App, appInfo)
+        if not ok then
+            log:error("Error launching %s: %s", appInfo.module, err)
+        end
+    else
+        log:error("Cannot load module %s: %s", appInfo.module, App)
+    end
 end
 
 local function main()
     local json = require("core.json")
-    local log = require("core.log"):new("launcher")
-
-    local appconfig = json.read("app-config.json")
-
-    if appconfig == nil then
-        log:error("Cannot read app-config.json. Launcher aborted.")
-        return
-    end
+    local configs = {json.read("app-config.json"), json.read("site-app-config.json")}
 
     log:info("Launcher module loaded")
-    for k, appInfo in pairs(appconfig) do
-        local ok, App = pcall(require, appInfo.module)
-        if ok then
-            ok, err = pcall(launchApp, App, appInfo)
-            if not ok then
-                log:error("Error launching %s: %s", appInfo.module, err)
+    local i = 0
+    for _, appconfig in pairs(configs) do
+        if appconfig ~= nil then
+            for _, appInfo in pairs(appconfig) do
+                launch(appInfo)
+                i = i + 1
             end
-        else
-            log:error("Cannot load module %s: %s", appInfo.module, App)
         end
+    end
+    if i == 0 then
+        log:warning("No applications were launched. Does app-config.json or site-app-config.json exist?")
+    else
+        log:info("%d application(s) launched", i)
     end
 end
 
 main()
+
+return Launcher
