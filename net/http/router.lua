b8760366ebd8b12854387be5b92ea3d8d9d3a5af
Javier Peletier <jm@epiclabs.io>
2019-11-06 15:50:45 +0100

    fw/net/http: Add method filtering to routes

--- net/http/router.lua
+++ net/http/router.lua
@@ -3,23 +3,36 @@ local processRoute
 
 processRoute = function(routes, req)
     for _, route in ipairs(routes) do
-        local matches = {string.match(req.request, route.pattern or ".*")}
-        if #matches > 0 then
-            if type(route.handler) == "function" then
-                local ok, func = pcall(route.handler, req, matches)
-                if not ok then
-                    return errorHandler(503, "Error running handler: " .. func)
+        local methodOk = false
+        if route.methods then
+            for _, method in ipairs(route.methods) do
+                if method == req.method then
+                    methodOk = true
+                    break
                 end
-                if type(func) == "function" then
-                    return func
+            end
+        else
+            methodOk = true
+        end
+        if methodOk then
+            local matches = {string.match(req.request, route.pattern or ".*")}
+            if #matches > 0 then
+                if type(route.handler) == "function" then
+                    local ok, func = pcall(route.handler, req, matches)
+                    if not ok then
+                        return errorHandler(500, "Error running handler: " .. func)
+                    end
+                    if type(func) == "function" then
+                        return func
+                    else
+                        --if a function is not returned, skip to the next handler
+                    end
                 else
-                    --if a function is not returned, skip to the next handler
-                end
-            else
-                if type(route.handler) == "table" then
-                    return processRoute(route.handler, req)
-                else
-                    return errorHandler(503, "Invalid route handler")
+                    if type(route.handler) == "table" then
+                        return processRoute(route.handler, req)
+                    else
+                        return errorHandler(503, "Invalid route handler")
+                    end
                 end
             end
         end
