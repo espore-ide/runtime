97e4bf4e8a4cec2bf45a040b3e7135ce9def27cf
Javier Peletier <jm@epiclabs.io>
2019-11-06 15:50:58 +0100

    fw/net/http: add JSON serve function

---
+++ net/http/json.lua
@@ -0,0 +1,9 @@
+-- Part of nodemcu-httpserver, handles sending JSON to client.
+-- Author: Javier Peletier
+
+return function(t)
+   return function(connection, req, args)
+      connection:sendHeader(200, "text/json")
+      connection:send(sjson.encode(t))
+   end
+end
