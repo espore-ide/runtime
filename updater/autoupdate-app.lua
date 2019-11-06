20e3655faf9029dda64d2a6c018a241c6d4cfe86
Javier Peletier <jm@epiclabs.io>
2019-11-06 17:44:06 +0100

    add auto updater app

---
+++ updater/autoupdate-app.lua
@@ -0,0 +1,76 @@
+local log = require("core.log"):new("autoupdate")
+local updater = require("updater.updater")
+
+local App = {}
+
+function App:new()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+--config:
+-- period: time in sec to check for updates
+
+function App:init(config)
+    self.timer = tmr:create()
+    log:info("Will check for updates every %d seconds", config.period)
+    self.timer:alarm(
+        config.period * 1000,
+        tmr.ALARM_AUTO,
+        function()
+            self:checkUpdates()
+        end
+    )
+    self.lastChecked = node.uptime()
+end
+
+function App:checkUpdates()
+    log:info("Checking for updates ...")
+    self.lastChecked = node.uptime()
+    updater.update(
+        function(result)
+            if type(result) == "string" then
+                log:error("Error while checking for updates: %s", result)
+            else
+                log:info("Update check finished successfully")
+            end
+        end
+    )
+end
+
+function App:terminate()
+    self.timer:stop()
+    self.timer:unregister()
+    self.timer = nil
+end
+
+function App:ui()
+    local this = self
+    if self._ui == nil then
+        self._ui = {
+            actions = {
+                {
+                    type = "button",
+                    label = "Check now",
+                    action = function()
+                        this:checkUpdates()
+                    end
+                }
+            },
+            dashboard = {
+                {
+                    type = "value",
+                    label = "Last checked (s ago)",
+                    value = function()
+                        return tostring(math.floor((node.uptime() - this.lastChecked) / 1000000))
+                    end
+                }
+            }
+        }
+    end
+    return self._ui
+end
+
+return App
