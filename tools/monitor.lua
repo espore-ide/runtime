4243c1ea46c5e8d5137678b3d582ca64e451d0fc
Javier Peletier <jm@epiclabs.io>
2019-12-02 16:24:41 +0100

    Report fw version via MQTT

--- tools/monitor.lua
+++ tools/monitor.lua
@@ -1,4 +1,4 @@
-local client = require("mqtt.service")
+local client = require("mqtt.service"):subclient("espore/" .. firmware.name)
 local wifi = require("wifi.manager")
 local pkg = require("core.pkg")
 local pformat = require("core.stringutil").pformat
@@ -12,6 +12,17 @@ return function(config)
         function()
             client:publish("sys/uptime", pformat("%d", math.floor(node.uptime() / 1000000)))
             client:publish("sys/free", pformat("%d", node.heap()))
+        end
+    )
+
+    client:runOnConnect(
+        function(reconnect)
+            client:publish("sys/version", firmware.version, 0, true)
+            client:publish("sys/wifi/ip", wifi.info.ip, 0, true)
+            client:publish("sys/wifi/mac", wifi.info.mac, 0, true)
+            client:publish("sys/wifi/ssid", wifi.info.ssid, 0, true)
+            client:publish("sys/wifi/gw", wifi.info.gw, 0, true)
+            client:publish("sys/wifi/netmask", wifi.info.netmask, 0, true)
         end
     )
 end
