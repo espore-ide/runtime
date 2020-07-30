b66cc06be3aede5fdabff32b1fe67d14296e364a
Javier Peletier <jm@epiclabs.io>
2020-07-30 19:16:15 +0200

    autoupdate: mqtt hooks and global function to update now

--- updater/autoupdate-app.lua
+++ updater/autoupdate-app.lua
@@ -1,6 +1,7 @@
 local log = require("core.log"):new("autoupdate")
 local updater = require("updater.updater")
 local mqtt = require("mqtt.service")
+local pformat = require("core.stringutil").pformat
 
 local App = {}
 
@@ -13,15 +14,20 @@ end
 
 -- config:
 -- period: time in sec to check for updates
-
 function App:init(config)
     self.timer = tmr:create()
+
+    local function checkUpdatesFromMQTT()
+        self:checkUpdates("mqtt message received")
+    end
+
     log:info("Will check for updates every %d seconds until MQTT connects",
              config.period)
     self.timer:alarm(config.period * 1000, tmr.ALARM_AUTO,
-                     function() self:checkUpdates() end)
-    mqtt:subscribe("espore/global/update/set", 0,
-                   function(data) self:checkUpdates() end)
+                     function() self:checkUpdates("timer tick") end)
+    mqtt:subscribe("espore/all/update/set", 0, checkUpdatesFromMQTT)
+    mqtt:subscribe(pformat("espore/%s/update/set", firmware.name), 0,
+                   checkUpdatesFromMQTT)
     mqtt:runOnConnect(function()
         self.timer:unregister()
         self.timer = nil
@@ -29,10 +35,11 @@ function App:init(config)
     end)
 
     self.lastChecked = node.uptime()
+    updateNow = function() self:checkUpdates("User command") end
 end
 
-function App:checkUpdates()
-    log:info("Checking for updates ...")
+function App:checkUpdates(reason)
+    log:info("Checking for updates due to %s", reason)
     self.lastChecked = node.uptime()
     updater.update(function(result)
         if type(result) == "string" then
@@ -47,33 +54,6 @@ function App:terminate()
     self.timer:stop()
     self.timer:unregister()
     self.timer = nil
-end
-
-function App:ui()
-    local this = self
-    if self._ui == nil then
-        self._ui = {
-            actions = {
-                {
-                    type = "button",
-                    label = "Check now",
-                    action = function() this:checkUpdates() end
-                }
-            },
-            dashboard = {
-                {
-                    type = "value",
-                    label = "Last checked (s ago)",
-                    value = function()
-                        return tostring(math.floor(
-                                            (node.uptime() - this.lastChecked) /
-                                                1000000))
-                    end
-                }
-            }
-        }
-    end
-    return self._ui
 end
 
 return App
